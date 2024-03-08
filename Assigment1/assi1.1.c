534e045a165661fa95249c4fa25705d5ada82a67
Ankita-Lengare <ankulengare0294@gmail.com>
2024-03-08 18:14:20 +0530

    Assignment1

---
+++ Assigment1/assi1.1.c
@@ -0,0 +1,19 @@
+
+
+
+#include<stdio.h>
+int main()
+      {
+	         int a,b,sum;
+             printf("Enter two no : \n");
+			 scanf("%d %d ",&a, &b);
+			 
+			 sum = a+b;
+			 printf(" sum of two no: %d ",sum);
+			 return 0;
+
+
+
+
+      }  
+
