d250f1cdc802b2bdadbdeb7ce0d82d0144ff1049
Ankita-Lengare <ankulengare0294@gmail.com>
2024-03-24 13:38:59 +0530

    assi4.1b

---
+++ Assignment4/assi4.1b.c
@@ -0,0 +1,29 @@
+//Write a function to calculate factorial of a given number using recursion.
+
+
+#include<stdio.h>
+int fact(int num);
+int main()
+{
+
+	int num,result;
+
+	printf("enter number : ");
+	scanf("%d",&num);
+
+	printf("factorial= %d\n",fact(num));
+return 0;
+}
+
+
+int fact (int num)
+{
+
+	if(num==1)
+	return 1;
+
+	else
+  	return num*fact(num-1);
+
+}
+
